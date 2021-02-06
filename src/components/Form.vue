6bba01a66260b548a223793952b9567e7a4da7d1
Michael <michael.d.long88@gmail.com>
2021-02-05 21:55:40 -0500

    Add form calculations and store methods

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,36 +1,47 @@
 <template>
   <div class="container mx-auto">
-    <form action="#" method="POST">
+    <form @submit.prevent="calculatePaymentAmounts">
         <div class="col-span-12 mx-6 md:mx-0">
           <div class="form-float-div">
-            <input type="number" v-model.number="loanAmount" name="loanAmount" placeholder=" " min="1" step=".01" class="form-float-input" required/>
+            <input type="number" v-model.number="loanAmount" name="loanAmount" :placeholder="getLoanAmount" min="1" step=".01" class="form-float-input" required/>
             <label for="loanAmount" class="form-float-label">Loan Amount</label>
           </div>
           <div class="form-float-div">
-            <input type="number" v-model.number="interestRate" name="annualInterestRate" placeholder=" " min="1" step=".01" class="form-float-input" required/>
+            <input type="number" v-model.number="interestRate" name="annualInterestRate" :placeholder="getAnnualInterestRate" min="1" step=".01" class="form-float-input" required/>
             <label for="annualInterestRate" class="form-float-label">Annual Interest Rate</label>
           </div>
           <div class="form-float-div">
-            <input type="number" v-model.number="loanPeriod" name="loanPeriod" placeholder=" " min="1" step="1" class="form-float-input" required/>
+            <input type="number" v-model.number="loanPeriod" name="loanPeriod" :placeholder="getLoanPeriod" min="1" step="1" class="form-float-input" required/>
             <label for="loanPeriod" class="form-float-label">Loan Period in Years</label>
           </div>
           <div class="form-float-div">
-            <input type="number" v-model.number="numberOfPayments" name="numberOfPayments" placeholder=" " min="1" step="1" max="12" class="form-float-input" required/>
+            <input type="number" v-model.number="numberOfPayments" name="numberOfPayments" :placeholder="getNumberOfPayments" min="1" step="1" max="12" class="form-float-input" required/>
             <label for="numberOfPayments" class="form-float-label">Number of Payments Per Year</label>
           </div>
           <div class="form-float-div">
-            <input type="text" v-model="startDate" name="loanStartDate" placeholder=" " class="form-float-input" onfocus="(this.type='date')" onfocusout="(this.type='text')" required/>
+            <input type="text" v-model="startDate" name="loanStartDate" :placeholder="getLoanStartDate" class="form-float-input" onfocus="(this.type='date')" onfocusout="(this.type='text')" required/>
             <label for="loanStartDate" class="form-float-label">Loan Start Date</label>
           </div>
           <div class="form-float-div">
-            <input type="number" v-model.number="extraPaymentAmount" name="optionalExtraPayments" placeholder=" " min="1" step="any" class="form-float-input"/>
+            <input type="number" v-model.number="extraPaymentAmount" name="optionalExtraPayments" :placeholder="getOptionalExtraPayments" min="1" step="any" class="form-float-input"/>
             <label for="optionalExtraPayments" class="form-float-label">Optional Extra Payments</label>
           </div>
         </div>
+        <input type="submit" value="Submit">
     </form>
+    <!-- <table v-if="payments">
+      <thead>
+        <tr v-for="header in Object.keys(calculatedPayments[0])" :key="header">
+          <th>
+            {{ header }}
+          </th>
+        </tr>
+      </thead>
+    </table> -->
   </div>
 </template>
 <script>
+import { mapActions, mapGetters } from 'vuex'
 export default {
   name: 'Form',
   data () {
@@ -40,7 +51,129 @@ export default {
       loanPeriod: null,
       numberOfPayments: null,
       startDate: null,
-      extraPaymentAmount: null
+      extraPaymentAmount: null,
+      payments: []
+    }
+  },
+  computed: {
+    ...mapGetters([
+      'getLoanAmount',
+      'getAnnualInterestRate',
+      'getLoanPeriod',
+      'getNumberOfPayments',
+      'getLoanStartDate',
+      'getOptionalExtraPayments'
+    ]),
+    calculatedPayments: function () {
+      if (this.payments) {
+        return this.payments
+      }
+      return [{
+        'Payment No.': 1,
+        'Payment Date': '1970-01-01',
+        'Beginning Balance': 0,
+        'Scheduled Payment': 0,
+        'Extra Payment': 0,
+        'Total Payment': 0,
+        Principal: 0,
+        Interest: 0,
+        'Ending Balance': 0,
+        'Cumulative Interest': 0
+      }]
+    }
+  },
+  methods: {
+    ...mapActions([
+      'setLoanAmount',
+      'setAnnualInterestRate',
+      'setLoanPeriod',
+      'setNumberOfPayments',
+      'setLoanStartDate',
+      'setOptionalExtraPayments',
+      'setPayments',
+      'setScheduledPayment',
+      'setScheduledNumberOfPayments',
+      'setActualNumberOfPayments',
+      'setTotalEarlyPayments',
+      'setTotalInterest']),
+    calculatePaymentAmounts: function () {
+      this.setLoanAmount({ loanAmount: this.loanAmount })
+      this.setAnnualInterestRate({ annualInterestRate: this.interestRate })
+      this.setLoanPeriod({ loanPeriod: this.loanPeriod })
+      this.setNumberOfPayments({ numberOfPayments: this.numberOfPayments })
+      this.setLoanStartDate({ loanStartDate: this.startDate })
+      this.setOptionalExtraPayments({ optionalExtraPayments: this.extraPaymentAmount })
+      const payments = []
+      const interestRate = this.interestRate / 100
+      const rate = interestRate / this.numberOfPayments
+      const totalNumberOfPayments = this.loanPeriod * this.numberOfPayments
+      this.setScheduledNumberOfPayments({ scheduledNumberOfPayments: totalNumberOfPayments })
+      const monthlyPayment = (this.loanAmount * rate) / (1 - ((1 + rate) ** (-1 * totalNumberOfPayments)))
+      this.setScheduledPayment({ scheduledPayment: monthlyPayment })
+      const splitDates = this.startDate.split('-')
+      const year = parseInt(splitDates[0])
+      const month = parseInt(splitDates[1])
+      const day = parseInt(splitDates[2])
+      let currentBalance = this.loanAmount
+      const extraPayment = this.extraPaymentAmount
+      let totalPayment = monthlyPayment
+      let currentYear = year
+      let cumulativeInterest = 0
+      let rowNum = 1
+      while (currentBalance > 0) {
+        let currentMonth = Math.floor(month + (rowNum * (12 / this.numberOfPayments)))
+        // console.log('currentMonth: ', currentMonth)
+        let yearAddition = 0
+        if (currentMonth >= 13) {
+          yearAddition = Math.floor(currentMonth / 12)
+          yearAddition = currentMonth % 12 === 0 ? yearAddition - 1 : yearAddition
+        }
+        // const yearAddition = Math.floor(currentMonth) % 12 === 0 ? 0 : Math.floor(currentMonth / 12)
+        // const yearAddition = currentMonth >= 13 ? Math.floor(currentMonth / 12) : 0
+        // console.log('yearAddition: ', yearAddition)
+        currentYear = (year + yearAddition) === currentYear ? currentYear : year + yearAddition
+        currentMonth = currentMonth >= 13 ? currentMonth % 12 : currentMonth
+        currentMonth = currentMonth === 0 ? 12 : currentMonth
+        let currentExtraPayment = 0
+        if (extraPayment && (extraPayment + monthlyPayment) < currentBalance) {
+          currentExtraPayment = extraPayment
+        } else if (extraPayment && (currentBalance - monthlyPayment) > 0) {
+          currentExtraPayment = currentBalance - monthlyPayment
+        }
+        totalPayment = (extraPayment + monthlyPayment) < currentBalance ? monthlyPayment + extraPayment : currentBalance
+        const interest = currentBalance * interestRate / this.numberOfPayments
+        const principal = totalPayment - interest
+        const endingBalance = (monthlyPayment + extraPayment) < currentBalance ? currentBalance - principal : 0
+        cumulativeInterest += interest
+        payments.push({
+          'Payment No.': rowNum,
+          'Payment Date': `${currentYear}-${currentMonth}-${day}`,
+          'Beginning Balance': currentBalance.toLocaleString('en-US', { style: 'currency', currency: 'USD' }),
+          'Scheduled Payment': monthlyPayment.toLocaleString('en-US', { style: 'currency', currency: 'USD' }),
+          'Extra Payment': currentExtraPayment.toLocaleString('en-US', { style: 'currency', currency: 'USD' }),
+          'Total Payment': totalPayment.toLocaleString('en-US', { style: 'currency', currency: 'USD' }),
+          Principal: principal.toLocaleString('en-US', { style: 'currency', currency: 'USD' }),
+          Interest: interest.toLocaleString('en-US', { style: 'currency', currency: 'USD' }),
+          'Ending Balance': endingBalance.toLocaleString('en-US', { style: 'currency', currency: 'USD' }),
+          'Cumulative Interest': cumulativeInterest.toLocaleString('en-US', { style: 'currency', currency: 'USD' })
+        })
+        currentBalance = endingBalance
+        rowNum++
+      }
+      this.setActualNumberOfPayments({ actualNumberOfPayments: rowNum })
+      console.table(payments)
+      this.payments = payments
+      this.setPayments({ payments: payments })
+      const totalEarlyPayments = payments.reduce((acc, paymentRow) => {
+        acc += paymentRow['Extra Payment']
+        return acc
+      }, 0)
+      const totalInterest = payments.reduce((acc, paymentRow) => {
+        acc += paymentRow.Interest
+        return acc
+      }, 0)
+      this.setTotalEarlyPayments({ totalEarlyPayments: totalEarlyPayments })
+      this.setTotalInterest({ totalInterest: totalInterest })
     }
   }
 }
--- src/store/index.js
+++ src/store/index.js
@@ -5,12 +5,60 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    theme: {}
+    theme: {},
+    loanAmount: ' ',
+    annualInterestRate: ' ',
+    loanPeriodInYears: ' ',
+    numberOfPayments: ' ',
+    loanStartDate: ' ',
+    optionalExtraPayments: ' ',
+    payments: [],
+    scheduledPayment: '',
+    scheduledNumberOfPayments: 0,
+    actualNumberOfPayments: 0,
+    totalEarlyPayments: 0,
+    totalInterest: 0
   },
   mutations: {
     SET_THEME (state, theme) {
       state.theme = theme
       localStorage.theme = theme
+    },
+    SET_LOAN_AMOUNT (state, loanAmount) {
+      state.loanAmount = loanAmount
+    },
+    SET_ANNUAL_INTEREST_RATE (state, annualInterestRate) {
+      state.annualInterestRate = annualInterestRate
+    },
+    SET_LOAN_PERIOD (state, loanPeriodInYears) {
+      state.loanPeriodInYears = loanPeriodInYears
+    },
+    SET_NUMBER_OF_PAYMENTS (state, numberOfPayments) {
+      state.numberOfPayments = numberOfPayments
+    },
+    SET_LOAN_START_DATE (state, loanStartDate) {
+      state.loanStartDate = loanStartDate
+    },
+    SET_OPTIONAL_EXTRA_PAYMENTS (state, optionalExtraPayments) {
+      state.optionalExtraPayments = optionalExtraPayments
+    },
+    SET_PAYMENTS (state, payments) {
+      state.payments = payments
+    },
+    SET_SCHEDULED_PAYMENT (state, scheduledPayment) {
+      state.scheduledPayment = scheduledPayment
+    },
+    SET_SCHEDULED_NUMBER_OF_PAYMENTS (state, scheduledNumberOfPayments) {
+      state.scheduledNumberOfPayments = scheduledNumberOfPayments
+    },
+    SET_ACTUAL_NUMBER_OF_PAYMENTS (state, actualNumberOfPayments) {
+      state.actualNumberOfPayments = actualNumberOfPayments
+    },
+    SET_TOTAL_EARLY_PAYMENTS (state, totalEarlyPayments) {
+      state.totalEarlyPayments = totalEarlyPayments
+    },
+    SET_TOTAL_INTEREST (state, totalInterest) {
+      state.totalInterest = totalInterest
     }
   },
   actions: {
@@ -36,11 +84,83 @@ export default new Vuex.Store({
           commit('SET_THEME', 'light')
           break
       }
+    },
+    setLoanAmount ({ commit }, payload) {
+      commit('SET_LOAN_AMOUNT', payload.loanAmount)
+    },
+    setAnnualInterestRate ({ commit }, payload) {
+      commit('SET_ANNUAL_INTEREST_RATE', payload.annualInterestRate)
+    },
+    setLoanPeriod ({ commit }, payload) {
+      commit('SET_LOAN_PERIOD', payload.loanPeriod)
+    },
+    setNumberOfPayments ({ commit }, payload) {
+      commit('SET_NUMBER_OF_PAYMENTS', payload.numberOfPayments)
+    },
+    setLoanStartDate ({ commit }, payload) {
+      commit('SET_LOAN_START_DATE', payload.loanStartDate)
+    },
+    setOptionalExtraPayments ({ commit }, payload) {
+      commit('SET_OPTIONAL_EXTRA_PAYMENTS', payload.optionalExtraPayments)
+    },
+    setPayments ({ commit }, payload) {
+      commit('SET_PAYMENTS', payload.payments)
+    },
+    setScheduledPayment ({ commit }, payload) {
+      commit('SET_SCHEDULED_PAYMENT', payload.scheduledPayment)
+    },
+    setScheduledNumberOfPayments ({ commit }, payload) {
+      commit('SET_SCHEDULED_NUMBER_OF_PAYMENTS', payload.scheduledNumberOfPayments)
+    },
+    setActualNumberOfPayments ({ commit }, payload) {
+      commit('SET_ACTUAL_NUMBER_OF_PAYMENTS', payload.actualNumberOfPayments)
+    },
+    setTotalEarlyPayments ({ commit }, payload) {
+      commit('SET_TOTAL_EARLY_PAYMENTS', payload.totalEarlyPayments)
+    },
+    setTotalInterest ({ commit }, payload) {
+      commit('SET_TOTAL_INTEREST', payload.totalInterest)
     }
   },
   getters: {
     getTheme: (state) => {
       return state.theme
+    },
+    getLoanAmount: state => {
+      return state.loanAmount
+    },
+    getAnnualInterestRate: state => {
+      return state.annualInterestRate
+    },
+    getLoanPeriod: state => {
+      return state.loanPeriodInYears
+    },
+    getNumberOfPayments: state => {
+      return state.numberOfPayments
+    },
+    getLoanStartDate: state => {
+      return state.loanStartDate
+    },
+    getOptionalExtraPayments: state => {
+      return state.optionalExtraPayments
+    },
+    getPayments: state => {
+      return state.payments
+    },
+    getScheduledPayment: state => {
+      return state.scheduledPayment
+    },
+    getScheduledNumberOfPayments: state => {
+      return state.scheduledNumberOfPayments
+    },
+    getActualNumberOfPayments: state => {
+      return state.actualNumberOfPayments
+    },
+    getTotalEarlyPayments: state => {
+      return state.totalEarlyPayments
+    },
+    getTotalInterest: state => {
+      return state.totalInterest
     }
   },
   modules: {
